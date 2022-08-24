e4efce918a5d340e67765184fa6e9cbce6f4fe4e
vaish139 <vaish139@gmail.com>
2022-08-24 11:48:59 +0530

    Ninja added

---
+++ src/components/Ninja.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Ninja extends Component {
+  render() {
+    return (
+      <div>
+        this is ninja component
+      </div>
+    )
+  }
+}
+
+export default Ninja
